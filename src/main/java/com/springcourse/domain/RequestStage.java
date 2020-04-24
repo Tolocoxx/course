66863cdd493c5c67c0c6fd599dde012476296e89
Matheus Freire <matheusfreire10@live.com>
2020-04-24 11:02:47 -0300

    Map entities

--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -6,18 +6,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "request_stage")
 public class RequestStage {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Column(name = "realization_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date realizationDate;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestState state;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
     private Request request;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 }
