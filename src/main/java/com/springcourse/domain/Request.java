0bd9592517043dbdd9d13ffe4d0668d4d67b96da
Matheus Freire <matheusfreire10@live.com>
2020-05-01 19:18:26 -0300

    Ignore stage during request serialization

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -1,5 +1,6 @@
 package com.springcourse.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.springcourse.domain.enums.RequestState;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -46,6 +47,7 @@ public class Request implements Serializable {
     @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
 
+    @Getter(onMethod = @__({@JsonIgnore}))
     @OneToMany(mappedBy = "request")
     private List<RequestStage> stages = new ArrayList<RequestStage>();
 
