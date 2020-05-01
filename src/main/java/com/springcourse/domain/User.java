8d5111197b840c04fc1bb668ea426fcb10bffdc5
Matheus Freire <matheusfreire10@live.com>
2020-05-01 19:19:17 -0300

    Ignore password, requests, stages during serialization

--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -1,5 +1,7 @@
 package com.springcourse.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.springcourse.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -33,6 +35,8 @@ public class User implements Serializable {
 	@Column(length = 75, nullable = false, unique = true)
 	private String email;
 
+	@Getter(onMethod = @__({@JsonIgnore}))
+	@Setter(onMethod = @__({@JsonProperty}))
 	@Column(length = 100, nullable = false)
 	private String password;
 
@@ -40,9 +44,11 @@ public class User implements Serializable {
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
+	@Getter(onMethod = @__({@JsonIgnore}))
 	@OneToMany(mappedBy = "owner")
 	private List<Request> requests = new ArrayList<Request>();
 
+	@Getter(onMethod = @__({@JsonIgnore}))
 	@OneToMany(mappedBy = "owner")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 
