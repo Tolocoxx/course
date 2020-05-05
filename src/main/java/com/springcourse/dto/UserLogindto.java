9aff11707d3ad74884499e0377e02ae8485d6281
Matheus Freire <matheusfreire10@live.com>
2020-05-04 22:13:28 -0300

    Add attribute validation

--- src/main/java/com/springcourse/dto/UserLogindto.java
+++ src/main/java/com/springcourse/dto/UserLogindto.java
@@ -5,11 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class UserLogindto {
+
+    @Email(message = "Invalid email addres")
     private String email;
+
+    @NotBlank(message = "Password required")
     private String password;
 }
--- src/main/java/com/springcourse/dto/UserUpdateRoleDTO.java
+++ src/main/java/com/springcourse/dto/UserUpdateRoleDTO.java
@@ -4,8 +4,12 @@ import com.springcourse.domain.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class UserUpdateRoleDTO {
+
+    @NotNull(message = "Role required")
     private Role role;
 }
