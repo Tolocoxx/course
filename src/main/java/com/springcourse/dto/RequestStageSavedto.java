9ddef52921099f394a9218215d9bb69080a740dd
Matheus Freire <matheusfreire10@live.com>
2020-05-05 07:40:23 -0300

    Create request stage save dto

---
+++ src/main/java/com/springcourse/dto/RequestStageSavedto.java
@@ -0,0 +1,36 @@
+package com.springcourse.dto;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.RequestState;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RequestStageSavedto {
+
+    private String description;
+
+    @NotNull(message = "State required")
+    private RequestState state;
+
+    @NotNull(message = "Request required")
+    private Request request;
+
+    @NotNull(message = "Owner required")
+    private User owner;
+
+    public RequestStage tranformToRequestStage(){
+        RequestStage stage = new RequestStage(null, this.description, null, this.state, this.request, this.owner);
+        return stage;
+    }
+
+}
