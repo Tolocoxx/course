8813deb90dff1bd8f79a2e1eb4fbcd53c61eb077
Matheus Freire <matheusfreire10@live.com>
2020-05-05 07:31:23 -0300

    Create request update dto

---
+++ src/main/java/com/springcourse/dto/RequestUpdatedto.java
@@ -0,0 +1,40 @@
+package com.springcourse.dto;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.RequestState;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RequestUpdatedto {
+    @NotBlank(message = "Subject required")
+    private String subject;
+
+    private String description;
+
+    @NotNull(message = "State required")
+    private RequestState state;
+
+    @NotNull(message = "Owner required")
+    private User owner;
+
+    private List<RequestStage> stages = new ArrayList<RequestStage>();
+
+    public Request transformeToRequest(){
+        Request request = new Request(null, this.subject, this.description, null, this.state, this.owner, this.stages);
+
+        return request;
+    }
+}
